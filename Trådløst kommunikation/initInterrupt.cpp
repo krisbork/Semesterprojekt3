bb540992af3a0edbe24dabdb865db83e81d55626
krisbork <70579756+krisbork@users.noreply.github.com>
2020-12-14 20:42:10 +0100

    Opdateret parkeringsprogram til at kunne afbryde parkering

---
+++ Trådløst kommunikation/initInterrupt.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main()
+{
+    int fd;
+    fd = open("/sys/class/gpio/gpio13/direction", O_WRONLY);
+    if (fd == -1)
+    {
+        close(fd);
+        fd = open("/sys/class/gpio/export", O_WRONLY);
+        write(fd, "13",3);
+        close(fd);
+    }
+    
+    fd = open("/sys/class/gpio/gpio13/direction", O_WRONLY);
+    write(fd,"out",4);
+    close(fd);
+    
+    return 0;
+}
